0407d1059ec15642aab1094c82b891cd592d4a0d
Maksym Babii <MaksymBabiiWork@gmail.com>
2023-05-17 14:28:02 +0300

    Updated main.ts added port and host

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
+    bufferLogs: true,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  const host = process.env.HOST || '0.0.0.0';
+
+  await app.listen(port, host);
 }
+
 bootstrap();
